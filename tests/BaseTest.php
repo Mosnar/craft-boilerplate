630a31417d9f4c120f4ba90768d2e25563d01721
Jason McCallister <themccallister@gmail.com>
2016-06-28 12:43:25 -0400

    Update styleci, and add tests classes

---
+++ tests/BaseTest.php
@@ -0,0 +1,57 @@
+<?php
+
+class BaseTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $baseUrl = 'http://craft.dev';
+
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl($this->baseUrl);
+    }
+
+    protected function visit($uri)
+    {
+        $this->url($uri);
+        return $this;
+    }
+
+    protected function seeTitle($title)
+    {
+        $this->assertEquals($title, $this->title());
+        return $this;
+    }
+
+    protected function wait($seconds)
+    {
+        sleep($seconds);
+        return $this;
+    }
+
+    protected function click($class)
+    {
+        $this->byCssSelector($class)->click();
+        return $this;
+    }
+
+    protected function clickXpath($path)
+    {
+        $this->byXPath($class)->click();
+        return $this;
+    }
+
+    protected function see($text, $tag = 'body')
+    {
+        $content = $this->byTag($tag)->text();
+        $this->assertContains($text, $content);
+        return $this;
+    }
+
+    protected function dontSee($text, $tag = 'body')
+    {
+        $content = $this->byTag($tag)->text();
+        $this->assertNotContains($text, $content);
+        return $this;
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ExampleTest extends BaseTest
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
