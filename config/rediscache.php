e8fdef08cc1845513f0f7bf982f63b091161dc58
Jason McCallister <themccallister@gmail.com>
2016-11-02 14:59:54 -0400

    Update composer, PHP requirement and cleanup (#2)
    * Update composer, PHP requirement and cleanup
    
    Also addresses #1
    
    * Add some basics for easier setup, update gulpfile for bower example
    
    * Add support for nginx and switch Procfile
    
    * Update composer lock
    
    * Add post-install-cmd
    
    * Update the composer.json script for correct path
    
    * Adding Redis support for sessions
    
    * Nitpick is getting me...
    
    * Update nitpick, remove required key
    
    * Add missing autoload.php
    
    * REmove the required again
    
    * Formatting
    
    * die and dump
    
    * Add the slash
    
    * Fix autoload include
    
    * Flipping logic around
    
    * Add the static task for gulp

--- config/rediscache.php
+++ config/rediscache.php
@@ -1,9 +1,21 @@
 <?php
 
+// if its local
+if (getenv('APP_ENV') == 'local') {
+    return [
+        'hostname' => getenv('REDIS_HOST') ?: 'redis-server',
+        'username' => getenv('REDIS_USER') ?: null,
+        'port' => getenv('REDIS_PORT') ?: null,
+        'password' => getenv('REDIS_PASS') ?: null,
+        'database' => 0,
+        'timeout' => null,
+    ];
+}
+
+// assume production/staging
 $redis = parse_url(getenv('REDIS_URL'));
 
 return [
-
     'hostname' => $redis['host'],
     'username' => $redis['user'],
     'port' => $redis['port'],
--- public/index.php
+++ public/index.php
@@ -4,7 +4,20 @@
 $craftPath = '../';
 
 // Do not edit below this line
-$path = rtrim($craftPath, '/').'/app/index.php';
+$path = rtrim($craftPath, '/') . '/app/index.php';
+
+// require composer autoload
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// if APP_ENV is defined, assume production
+if (is_null(getenv('APP_ENV')) || getenv('APP_ENV') == 'local') {
+    try {
+        $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+        $dotenv->load();
+    } catch (Exception $e) {
+        exit('Missing the environment configuration');
+    }
+}
 
 // define the path the the templates
 define('CRAFT_TEMPLATES_PATH', '../resources/templates/');
@@ -14,7 +27,7 @@ if (!is_file($path)) {
         http_response_code(503);
     }
 
-    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in ' . __FILE__);
 }
 
 require_once $path;
